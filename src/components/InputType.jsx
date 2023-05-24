486abf57e6ad77765959e06227a61c37f49c88eb
asish <asish0sahu84@gmail.com>
2023-05-24 06:19:11 -0700

    touchtype

---
+++ src/components/InputType.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Random from "./Random";
+
+import Compare from "./compare";
+
+const InputType = () => {
+  return (
+    <>
+      <Random />
+      <Compare />
+    </>
+  );
+};
+
+export default InputType;
